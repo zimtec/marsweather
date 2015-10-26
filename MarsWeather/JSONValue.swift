563473489241fd08096ed2f308256089a006a48d
Claus Zimmermann <claus.zimmermann@zim-it.de>
2015-10-27 00:39:05 +0100

    First Commit
    Signed-off-by: Claus Zimmermann <claus.zimmermann@zim-it.de>

---
+++ MarsWeather/JSONValue.swift
@@ -0,0 +1,9 @@
+//
+//  JSONValue.swift
+//  MarsWeather
+//
+//  Created by Claus Zimmermann on 26.10.15.
+//  Copyright © 2015 Claus Zimmermann. All rights reserved.
+//
+
+import Foundation
---
+++ MarsWeather/MarsWeatherDownloadManager.swift
@@ -0,0 +1,9 @@
+//
+//  MarsWeatherDownloadManager.swift
+//  MarsWeather
+//
+//  Created by Claus Zimmermann on 26.10.15.
+//  Copyright © 2015 Claus Zimmermann. All rights reserved.
+//
+
+import Foundation
